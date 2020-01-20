8e9142869ed5af64b6a4ce57b077bb60a135c6af
Michael Grandison <african.genetic.survival@gmail.com>
2020-01-20 12:32:43 -0500

    index.js bootstrap loaded. navbar fixed. added some rows and columns

--- glamour_tours/src/App.js
+++ glamour_tours/src/App.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
+import MainNavigation from './components/MainNavigation';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import logo from './logo.svg';
 
 import './App.css';
@@ -21,20 +25,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <a
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           Learn React
-         </a>
-       </header>
+        <div className="MainBgLayer">
+        <MainNavigation />
+           <header className="App-header">
+             <img src={logo} className="App-logo" alt="logo" />
+             <p>
+               Edit <code>src/App.js</code> and save to yuh mah.
+             </p>
+           </header>
+           <Container className="MainContainer">
+            <Row className="MainRow">
+              <Col className="MainCol">1 of 3</Col>
+              <Col className="MainCol">2 of 3</Col>
+              <Col className="MainCol">3 of 3</Col>
+            </Row>
+            <Row className="MainFooterRow">
+              <Col className="MainCol">Footer</Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     );
   }
