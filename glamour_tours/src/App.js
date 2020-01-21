d7ecc2a0d13701e20c779734e4a28dcb18f1de3d
Michael Grandison <african.genetic.survival@gmail.com>
2020-01-20 23:01:10 -0500

    more bootstarp scaffolding

--- glamour_tours/src/App.js
+++ glamour_tours/src/App.js
@@ -1,48 +1,102 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-import MainNavigation from './components/MainNavigation';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import logo from './logo.svg';
 
+import MainNavigation from './components/MainNavigation/MainNavigation';
+import Company from './components/Company/Company';
+
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {
-    test: "test state"
+    test: "test state",
+    contentOn: false,
+    subMenuOn: false,
+    mainMenuItem: null,
+    subMenuItem: null,
+    contentItem: null,
+    hero: true,
+    loading: false,
   };
 
   constructor(props) {
     super(props);
-    this.args = "test prop"
+    this.test = "test prop"
   }
 
   componentDidMount() {
     console.log("It's Aliiive!!!");
   }
 
+  mainMenuSelect = (args) => {
+    console.log(`
+      NavBar Item Selected..
+      ${JSON.stringify(args)}
+      `);
+
+      this.setState({
+        subMenuOn: true,
+        contentOn: true,
+        subMenuItem: args,
+        contentItem: args,
+        hero: false })
+  }
+
+  returnHome = () => {
+    console.log(`
+      returning home...
+      `);
+
+      this.setState({
+        subMenuOn: false,
+        contentOn: false,
+        hero: true
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="MainBgLayer">
-        <MainNavigation />
-           <header className="App-header">
-             <img src={logo} className="App-logo" alt="logo" />
-             <p>
-               Edit <code>src/App.js</code> and save to yuh mah.
-             </p>
-           </header>
-           <Container className="MainContainer">
+        <MainNavigation
+          onMainMenuSelect={this.mainMenuSelect}
+          onReturnHome={this.returnHome}
+        />
+          <div className="MainDiv">
+            {this.state.hero === true && (
+            <Row className="HeaderRow">
+              <Col lg={9} className="MainCol">
+                Hero Image
+              </Col>
+              <Col lg={3} className="MainCol">
+                Hero Text
+              </Col>
+            </Row>
+            )}
+
             <Row className="MainRow">
-              <Col className="MainCol">1 of 3</Col>
-              <Col className="MainCol">2 of 3</Col>
-              <Col className="MainCol">3 of 3</Col>
+
+
+              {this.state.contentOn === true && (
+                <Col className="MainCol MainColContent">
+                  {this.state.contentItem === "company" && (
+                    <Company />
+                  )}
+                  {this.state.contentItem === "services" && (
+                    <p>Services Content</p>
+                  )}
+                </Col>
+              )}
+
             </Row>
+
             <Row className="MainFooterRow">
-              <Col className="MainCol">Footer</Col>
+              <Col className="MainCol FooterCol">Footer</Col>
             </Row>
-          </Container>
+          </div>
         </div>
       </div>
     );
---
+++ glamour_tours/src/components/MainNavigation/MainNavigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import './MainNavigation.css';
+
+const mainNavigation = (props) =>{
+
+  return (
+    <Navbar expand="lg" className="MainNav">
+      <Navbar.Brand onClick={props.onReturnHome} href="#home">Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link onClick={props.onMainMenuSelect.bind(this, "company")} href="#home">Company</Nav.Link>
+          <Nav.Link onClick={props.onMainMenuSelect.bind(this, "services")} href="#link">Services</Nav.Link>
+          <Nav.Link onClick={props.onMainMenuSelect.bind(this, "destination")} href="#link">Destination</Nav.Link>
+          <Nav.Link onClick={props.onMainMenuSelect.bind(this, "news")} href="#link">News</Nav.Link>
+          <Nav.Link onClick={props.onMainMenuSelect.bind(this, "gallery")} href="#link">Gallery</Nav.Link>
+          <Nav.Link onClick={props.onMainMenuSelect.bind(this, "contact")} href="#link">Contact</Nav.Link>
+
+          {
+            // <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            //   <NavDropdown.Item value="company" href="#action/3.1" onClick={props.onMainMenuSelect.bind(this)}>Company</NavDropdown.Item>
+            //   <NavDropdown.Item value="services" href="#action/3.2">Another action</NavDropdown.Item>
+            //   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            //   <NavDropdown.Divider />
+            //   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            // </NavDropdown>
+          }
+
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default mainNavigation;
